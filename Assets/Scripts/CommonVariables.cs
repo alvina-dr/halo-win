2e25ee340e803734c79c154aa9474f56a2661690
alvina-dr <alvinadr24@gmail.com>
2021-10-26 10:35:57 +0200

    ajout de la variable bonbon

---
+++ Assets/Scripts/CommonVariables.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CommonVariables : MonoBehaviour
+{
+
+    public int candyCount = 0;
+    public Text candyCountText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        candyCountText.text = "" + candyCount;
+        if (Input.GetKeyDown("space")) { //Pour tester à enlever plus tard
+            candyCount += 10;
+        }
+        
+    }
+
+    void addCandy(int number) {
+        candyCount += number;
+    }
+
+    void subCandy(int number) {
+        candyCount -= number;
+    }
+}
