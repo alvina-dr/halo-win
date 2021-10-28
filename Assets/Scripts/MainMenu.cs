2e0008f59a9fa212899a0c78912801f13f7b0b83
najouille <najadalmagne@gmail.com>
2021-10-28 15:09:09 +0200

    UI Shop

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,7 +10,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame (){
         
-        StartCoroutine(SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1));
+        // StartCoroutine(SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1));
     }
 
     IEnumerator LoadScene(int levelIndex){
