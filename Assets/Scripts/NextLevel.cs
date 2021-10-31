5ce3f11536f07b0509a8a2f9647144391a069bcd
najouille <najadalmagne@gmail.com>
2021-10-31 15:50:42 +0100

    added rework zombies, waves and Win and Defeat UI

--- Assets/Scripts/NextLevel.cs
+++ Assets/Scripts/NextLevel.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.UI;
 
 public class NextLevel : MonoBehaviour
 {
-
     GameObject noDestroy;
     public void goToNextLevel() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
 
     public void goToMainMenu() {
         noDestroy = GameObject.FindGameObjectWithTag("NoDestroy");
